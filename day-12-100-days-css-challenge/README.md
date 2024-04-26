4e62a8d7ec4fd4efe5a44c2ed9919e0322e99078
nilsertle <nilssertle@gmail.com>
2024-04-26 20:03:42 +0200

    adds day 12

---
+++ day-12-100-days-css-challenge/README.md
@@ -0,0 +1,4 @@
+# Day #12 - 100 Days CSS Challenge
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/nilsertle/pen/BaEbVmV](https://codepen.io/nilsertle/pen/BaEbVmV).
+
---
+++ day-12-100-days-css-challenge/src/script.js
@@ -0,0 +1,3 @@
+import FortawesomeFontawesomeFree from "https://esm.sh/@fortawesome/fontawesome-free";
+import fontAwesome from "https://esm.sh/font-awesome";
+import fontAwesome from "https://esm.sh/font-awesome";
